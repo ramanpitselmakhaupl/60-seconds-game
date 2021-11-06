be0949bed205208e6f728541c66de9603e4778bc
Raman Pitselmakhau <ramanpitselmakhaupl@gmail.com>
2021-11-06 18:52:42 +0300

    test

--- Main/NewDay.cs
+++ Main/NewDay.cs
@@ -13,7 +13,7 @@ namespace Main
         {
             int day = i; //day counter
             int rand = rndm;
-        
+        // hi world
 
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("                                       Day {0} ", day);
